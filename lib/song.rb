a95336b4783a73d75782d23621b2a74282922c7e
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-08-18 23:35:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   def self.genre_count
     @genre_count = {}
     @@genres.each do |genre|
-      @genre_count[genre] => @@genres.count(genre)
+      @genre_count[genre] = @@genres.count(genre)
     end
     @genre_count
   end
