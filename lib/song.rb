a0e39cf4ff2707c7089ed4e5a076ca631465f113
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-08-18 23:24:22 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :genre, :artist
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+  
+end
